79b0fca7322d339b05dd497894b781a994a76ad3
jacobl <jmlake569@gmail.com>
2020-09-23 12:13:09 -0400

    third

--- smart_check_auto_run.sh
+++ smart_check_auto_run.sh
@@ -4,7 +4,8 @@ RESULT=$?
 if [ $RESULT == 1 ]; then
     eksctl delete cluster --region=us-east-1 --name=smartcheck
 else
-    ./smartcheckon
+ ./smartcheckon
     export SERVICE_IP=$(kubectl get svc proxy -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+    sleep 5s
     echo Smart Check URL: https://$SERVICE_IP:443/
 fi
